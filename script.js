2478f131ebc19a48b5ad3e3af8a6605a39a19133
koogawa <koogawa.app@gmail.com>
2020-05-10 10:38:08 +0900

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+var cheers1 = document.getElementById("cheers1");
+var cheers2 = document.getElementById("cheers2");
+var laughter1 = document.getElementById("laughter1");
+var laughter2 = document.getElementById("laughter2");
+var laughter3 = document.getElementById("laughter3");
+var applause1 = document.getElementById("applause1");
+var applause2 = document.getElementById("applause2");
+
+function playCheers1() {
+    cheers1.currentTime = 0;
+    cheers1.play()
+}
+
+function playCheers2() {
+    cheers2.currentTime = 0;
+    cheers2.play()
+}
+
+function playLaughter1() {
+    laughter1.currentTime = 0;
+    laughter1.play()
+}
+
+function playLaughter2() {
+    laughter2.currentTime = 0;
+    laughter2.play()
+}
+
+function playLaughter3() {
+    laughter3.currentTime = 0;
+    laughter3.play()
+}
+
+function playApplause1() {
+    applause1.currentTime = 0;
+    applause1.play()
+}
+
+function playApplause2() {
+    applause2.currentTime = 0;
+    applause2.play()
+}
